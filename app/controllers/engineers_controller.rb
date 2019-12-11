5a3be4cd5e4ee129b91d7ee090e4e70ec5ff4ecc
fut0k0 <cccabell@icloud.com>
2019-12-11 14:52:33 -0800

    set up namespaces

--- app/controllers/engineers_controller.rb
+++
@@ -1,2 +0,0 @@
-class EngineersController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :albums
-  resources :engineers
-  resources :mixers
-  resources :producers
-  resources :artists
-  resources :genres
-  resources :years
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	namespace :api do
+		namespace :v1 do
+			resources :albums
+			resources :engineers
+			resources :mixers
+			resources :producers
+			resources :artists
+			resources :genres
+			resources :years
+			resources :users
+		end
+	end
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
