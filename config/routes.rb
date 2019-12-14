8fa2d6aa089f3f6cdbd62b8c554229a6f9469b2b
fut0k0 <cccabell@icloud.com>
2019-12-13 17:16:15 -0800

    add routes for signup, login

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 			resources :genres
 			resources :years
 			resources :users
+			
+			post "login", to: "sessions#create"
+			post "signup", to: "users#create"
 		end
 	end
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
