8943c8112c5e2f8bf9337c9a3ee0604dacbaef29
fut0k0 <cccabell@icloud.com>
2019-12-13 17:16:55 -0800

    start work on users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,15 @@ class Api::V1::UsersController < ApplicationController
 	def index
 		render json: User.all
 	end
+	
+	def create
+		byebug
+		user = User.create(user_params)
+	end
+	
+	private
+	
+		def user_params
+			params.require(:user).permit(:username, :password)
+		end
 end
