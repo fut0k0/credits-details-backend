2d8f1d93cfd0871c26487ec39e6cbe89081af7f5
fut0k0 <cccabell@icloud.com>
2019-12-18 17:24:49 -0800

    add helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
 	include ActionController::Cookies
-# 	include ::ActionController::Cookies
+
+	def current_user
+		User.find_by(id: session[:current_user_id])
+	end
+
+	def is_logged_in
+		!!current_user
+	end
 end
