5d73371becba0863b7c69e19219a85906df1887c
fut0k0 <cccabell@icloud.com>
2020-01-20 15:12:52 -0800

    update producer model & controller

--- app/controllers/api/v1/producers_controller.rb
+++ app/controllers/api/v1/producers_controller.rb
@@ -2,4 +2,22 @@ class Api::V1::ProducersController < ApplicationController
 	def index
 		render json: Producer.all
 	end
+
+	def create
+		producer = Producer.create(producer_params)
+
+		if !producer.errors.any?
+			render json: producer
+		elsif producer.errors.messages[:name][0] == "has already been taken"
+			render json: {message: "Producer already exists"}
+		else
+			render json: {message: "Error occurred"}
+		end
+	end
+
+	private
+
+		def producer_params
+			params.require(:producer).permit(:name)
+		end
 end
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,3 +1,4 @@
 class Producer < ApplicationRecord
 	has_many :albums
+	validates :name, uniqueness: true
 end
