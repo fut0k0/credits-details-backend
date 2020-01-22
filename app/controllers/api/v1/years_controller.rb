89425f86d6957555c73f3c7be24c57438803fc11
fut0k0 <cccabell@icloud.com>
2020-01-21 16:00:32 -0800

    update year model & controller

--- app/controllers/api/v1/years_controller.rb
+++ app/controllers/api/v1/years_controller.rb
@@ -2,4 +2,22 @@ class Api::V1::YearsController < ApplicationController
 	def index
 		render json: Year.all
 	end
+
+	def create
+		year = Year.create(year_params)
+
+		if !year.errors.any?
+			render json: year
+		elsif year.errors.messages[:name][0] == "has already been taken"
+			render json: {message: "Year already exists"}
+		else
+			render json: {message: "Error occurred"}
+		end
+	end
+
+	private
+
+		def year_params
+			params.require(:year).permit(:name)
+		end
 end
--- app/models/year.rb
+++ app/models/year.rb
@@ -1,3 +1,4 @@
 class Year < ApplicationRecord
 	has_many :albums
+	validates :name, uniqueness: true
 end
