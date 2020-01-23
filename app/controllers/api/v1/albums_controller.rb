666221f282de34a003e41cb00b555bf0aebd2b3b
fut0k0 <cccabell@icloud.com>
2020-01-22 16:18:27 -0800

    update albums controller

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -2,4 +2,22 @@ class Api::V1::AlbumsController < ApplicationController
 	def index
 		render json: Album.all
 	end
+
+	def create
+		album = Album.create(album_params)
+
+byebug
+
+		if !album.errors.any?
+			render json: album
+		else
+			render json: {message: "Error occurred"}
+		end
+	end
+
+	private
+
+		def album_params
+			params.require(:album).permit(:title, :format, :user_id, :artist_id, :year_id, :genre_id, :producer_id, :mixer_id, :engineer_id)
+		end
 end
