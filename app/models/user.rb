29b81f266d86b96dacb4c1b58e2fba632be4604f
fut0k0 <cccabell@icloud.com>
2020-01-29 15:19:38 -0800

    update user model & serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
 	has_secure_password
 	has_many :albums
+	has_many :artists, -> { distinct }, through: :albums
+	has_many :years, -> { distinct }, through: :albums
+	has_many :genres, -> { distinct }, through: :albums
+	has_many :producers, -> { distinct }, through: :albums
+	has_many :mixers, -> { distinct }, through: :albums
+	has_many :engineers, -> { distinct }, through: :albums
 	validates :username, uniqueness: true
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
 	attributes :id, :username
 	has_many :albums
+	has_many :artists, through: :albums
+	has_many :years, through: :albums
+	has_many :genres, through: :albums
+	has_many :producers, through: :albums
+	has_many :mixers, through: :albums
+	has_many :engineers, through: :albums
 end
